c9eb062b41ccf334db532482650265fa33d46a9a
Dmitry Pankrashov <dpankrashov@centrvd.ru>
2022-06-27 21:37:25 +0300

    guest book 27/06/2022

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action="/" method="post" enctype="application/x-www-form-urlencoded">
+<form action="/" method="post" enctype="multipart/form-data">
     <div>
         <label for="user">User:</label>
         <input type="text" name="user" />
@@ -8,12 +8,14 @@
         <input type="text" name="message" />
     </div>
     <div>
+        <input type="file" name="picture" />
+    </div>
+    <div>
         <input type="hidden" value="eYIiRhAay8" name="form-token">
         <input type="submit" value="Log In" />
     </div>
 </form>
 <?php
-    var_dump($_REQUEST);
     if (array_key_exists('user',$_REQUEST) && array_key_exists('message',$_REQUEST) && $_REQUEST['form-token'] === 'eYIiRhAay8')
     {
         // проверка имени пользователя
@@ -28,9 +30,21 @@
             $messages = json_decode(file_get_contents('messages.json'),true);
         }
 
+        if(!empty($_FILES['picture']))
+        {
+            $name = basename($_FILES["picture"]['name']);
+            $ext = pathinfo($name,PATHINFO_EXTENSION);
+            if(!in_array($ext,['txt','jpg','pdf']))
+                die('Загружаемый файл имеет неподдерживаемое расширение!');
+            $name = uniqid().$ext;
+            move_uploaded_file($_FILES["picture"]['tmp_name'], "$name");
+            $picture = $name;
+        }
+
         $messages[] = [
             'user' => $_REQUEST['user'],
-            'message' => $_REQUEST['message']
+            'message' => $_REQUEST['message'],
+            'picture' => isset($picture) ? $picture : ''
         ];
 
         file_put_contents('messages.json',json_encode($messages));
@@ -42,5 +56,9 @@
         foreach ($messages as $message) {
             $message['message'] = htmlspecialchars($message['message']);
             echo "<div>{$message['user']} -> {$message['message']}</div>";
+            if(array_key_exists('picture', $message))
+            {
+                echo "<div><a href='/{$message['picture']}'>{$message['picture']}</a></div>";
+            }
         }
     }
