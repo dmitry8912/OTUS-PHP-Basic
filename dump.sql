d8696d0edcf20cb30d0d78a07929117f93e4fe66
Dmitry Pankrashov <dpankrashov@centrvd.ru>
2022-07-04 21:33:12 +0300

    guest book 04/07/2022 OTUS

--- dump.sql
+++ dump.sql
@@ -20,38 +20,40 @@ USE `otus17`;
 -- Дамп структуры для таблица otus17.messages
 CREATE TABLE IF NOT EXISTS `messages` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `user` varchar(250) NOT NULL DEFAULT '',
+  `user_id` bigint NOT NULL DEFAULT '0',
   `message` text NOT NULL,
   `picture` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `user_id_fk` (`user_id`),
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы otus17.messages: ~0 rows (приблизительно)
+-- Дамп данных таблицы otus17.messages: ~6 rows (приблизительно)
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` (`id`, `user`, `message`, `picture`, `created_at`) VALUES
-	(5, 'dmitry', 'test', 'carbon.png', '2022-07-01 21:08:31'),
-	(6, 'ivan', 'test2', NULL, '2022-07-01 21:08:39'),
-	(7, 'dmitry', '123', NULL, '2022-07-01 21:27:07'),
-	(8, 'dmitry', '321', NULL, '2022-07-01 21:27:16'),
-	(9, 'anton', 'privet!', NULL, '2022-07-01 21:27:36'),
-	(10, 'dmitry', '123', '62bf3ce04c55b.png', '2022-07-01 21:28:48');
+INSERT INTO `messages` (`id`, `user_id`, `message`, `picture`, `created_at`) VALUES
+	(11, 14, 'test', NULL, '2022-07-04 21:08:26'),
+	(12, 14, 'test#1', NULL, '2022-07-04 21:18:33'),
+	(13, 14, 'test#2', NULL, '2022-07-04 21:18:38'),
+	(14, 14, 'test#3', NULL, '2022-07-04 21:18:42'),
+	(15, 14, 'test#45', '62c32f109cc8a.png', '2022-07-04 21:18:56'),
+	(16, 14, 'ksugfiudsf', NULL, '2022-07-04 21:20:54');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 
 -- Дамп структуры для таблица otus17.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(250) NOT NULL DEFAULT '0',
-  `email` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `remember_token` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы otus17.users: ~0 rows (приблизительно)
+-- Дамп данных таблицы otus17.users: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `username`, `email`, `created_at`) VALUES
-	(10, 'dmitry1@gmail.com', 'dmitry1@gmail.com', '2022-07-01 20:39:17'),
-	(11, 'dmitry2@gmail.com', 'dmitry2@gmail.com', '2022-07-01 20:38:17');
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `remember_token`) VALUES
+	(14, 'admin', 'admin', '2022-07-04 21:08:10', '62c331892a6f4');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
--- index.php
+++ index.php
@@ -1,21 +1,12 @@
-<form action="/" method="post" enctype="multipart/form-data">
-    <div>
-        <label for="user">User:</label>
-        <input type="text" name="user" />
-    </div>
-    <div>
-        <label for="message">Message:</label>
-        <input type="text" name="message" />
-    </div>
-    <div>
-        <input type="file" name="picture" />
-    </div>
-    <div>
-        <input type="hidden" value="eYIiRhAay8" name="form-token">
-        <input type="submit" value="Log In" />
-    </div>
-</form>
 <?php
+    session_start();
+    var_dump($_COOKIE);
+    if(!empty($_COOKIE['remember-token'])) {
+        $userData = getUserDataByToken($_COOKIE['remember-token']);
+        $_SESSION['user_id'] = intval($userData['id']);
+        $_SESSION['username'] = $userData['username'];
+    }
+
     function connect() {
         return new PDO('mysql:host=localhost;dbname=otus17','root','root',[
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -24,39 +15,120 @@
 
     function getMessages() {
         $db = connect();
-        $result = $db->query("SELECT * FROM messages order by created_at DESC");
+        $result = $db->query("SELECT messages.id, users.username AS user, messages.message, messages.picture, messages.created_at FROM messages JOIN users ON users.id = messages.user_id ");
         $result->execute();
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    function addMessage(string $user, string $message, string $picture = null)
+    function addMessage(int $user_id, string $message, string $picture = null)
     {
         $db = connect();
-        $result = $db->prepare("INSERT INTO messages(user,message,picture) values (?,?,?)");
-        $result->execute([$user, $message, $picture]);
+        $result = $db->prepare("INSERT INTO messages(user_id,message,picture) values (?,?,?)");
+        $result->execute([$user_id, $message, $picture]);
     }
 
-    if (array_key_exists('user',$_REQUEST) && array_key_exists('message',$_REQUEST) && $_REQUEST['form-token'] === 'eYIiRhAay8')
+    function setRememberToken(int $user_id, string $token)
     {
-        // проверка имени пользователя
-        if (!preg_match("/^[a-zA-Zа-яА-Я]{3,20}+$/", $_REQUEST['user'])) {
-            die('Имя пользователя не соответствует формату!');
-        }
-
-        if(!empty($_FILES['picture']) && $_FILES['picture']['size'] > 0)
-        {
-            $name = basename($_FILES["picture"]['name']);
-            $ext = pathinfo($name,PATHINFO_EXTENSION);
-            if(!in_array($ext,['txt','jpg','pdf','png']))
-                die('Загружаемый файл имеет неподдерживаемое расширение!');
-            $name = uniqid().".".$ext;
-            move_uploaded_file($_FILES["picture"]['tmp_name'], "$name");
-            $picture = $name;
-        }
-
-        addMessage($_REQUEST['user'], $_REQUEST['message'], $picture ?? null);
+        $db = connect();
+        $result = $db->prepare("update users set remember_token = ? where id = ?");
+        $result->execute([$token, $user_id]);
     }
 
+    function getUserDataByToken(string $token)
+    {
+        $db = connect();
+        $result = $db->prepare("select username, id from users where remember_token = ?");
+        $result->execute([$token]);
+        if($result->rowCount() > 0) {
+            return $result->fetch(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
+    function authorize(string $user, string $password) {
+        $db = connect();
+        $result = $db->prepare("SELECT id from users where username = ? and password = ?");
+        $result->execute([$user, $password]);
+        if($result->rowCount() == 0) {
+            return false;
+        }
+        return $result->fetch(PDO::FETCH_ASSOC);
+    }
+
+    if(!empty($_GET['action']) && $_GET['action'] === 'auth')
+    {
+        $authResult = authorize($_POST['user'], $_POST['password']);
+        if(!$authResult) {
+            echo "<h2>Неправильное имя пользователя или пароль!</h2>";
+        } else {
+            $_SESSION['user_id'] = intval($authResult['id']);
+            $_SESSION['username'] = $_POST['user'];
+            if($_POST['remember_me'] === 'on') {
+                $token = uniqid();
+                setRememberToken($_SESSION['user_id'], $token);
+                setcookie('remember-token', $token, time() + 3600*24*30*6);
+            }
+        };
+    }
+
+    if(!empty($_GET['action']) && $_GET['action'] === 'addMessage')
+    {
+        if (array_key_exists('message',$_REQUEST) && $_REQUEST['form-token'] === 'eYIiRhAay8')
+        {
+            // проверка имени пользователя
+            /*if (!preg_match("/^[a-zA-Zа-яА-Я]{3,20}+$/", $_REQUEST['user'])) {
+                die('Имя пользователя не соответствует формату!');
+            }*/
+
+            if(!empty($_FILES['picture']) && $_FILES['picture']['size'] > 0)
+            {
+                $name = basename($_FILES["picture"]['name']);
+                $ext = pathinfo($name,PATHINFO_EXTENSION);
+                if(!in_array($ext,['txt','jpg','pdf','png']))
+                    die('Загружаемый файл имеет неподдерживаемое расширение!');
+                $name = uniqid().".".$ext;
+                move_uploaded_file($_FILES["picture"]['tmp_name'], "$name");
+                $picture = $name;
+            }
+
+            addMessage($_SESSION['user_id'], $_REQUEST['message'], $picture ?? null);
+        }
+    }
+?>
+<?php if(empty($_SESSION['user_id'])): ?>
+    <form action="/index.php?action=auth" method="post" enctype="multipart/form-data">
+        <div>
+            <label for="user">Username:</label>
+            <input type="text" name="user" />
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" name="password" />
+        </div>
+        <div>
+            <label for="remember_me">Remember me:</label>
+            <input type="checkbox" name="remember_me" />
+        </div>
+        <div>
+            <input type="submit" value="Log In" />
+        </div>
+    </form>
+<?php else: ?>
+    <form action="/index.php?action=addMessage" method="post" enctype="multipart/form-data">
+        <div>
+            <label for="message">Message:</label>
+            <input type="text" name="message" />
+        </div>
+        <div>
+            <input type="file" name="picture" />
+        </div>
+        <div>
+            <input type="hidden" value="eYIiRhAay8" name="form-token">
+            <input type="submit" value="Add message" />
+        </div>
+    </form>
+<?php endif; ?>
+<?php
     $messages = getMessages();
     foreach ($messages as $message) {
         $message['message'] = htmlspecialchars($message['message']);
